2718e3d25c524c8c84c40f944406313b81807176
Andrew <andrewchess25@gmail.com>
2019-10-03 00:05:50 +0300

    Data context and db initializer were added.

---
+++ support_tracker/support_tracker/Data_Layer/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using support_issue_tracker.Models;
+
+namespace support_issue_tracker.DbLayer
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base()
+        {
+            Database.SetInitializer(new DbInitializer());
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketStatus> TicketStatuses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
---
+++ support_tracker/support_tracker/Data_Layer/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using support_issue_tracker.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+
+namespace support_issue_tracker.DbLayer
+{
+    public class DbInitializer : CreateDatabaseIfNotExists<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            IList<Department> departments = new List<Department>();
+            departments.Add(new Department { DepartmentName = "Technicians" });
+            departments.Add(new Department { DepartmentName = "Repairmans" });
+            departments.Add(new Department { DepartmentName = "Support" });
+            context.Departments.AddRange(departments);
+            base.Seed(context);
+        }
+    }
+}
