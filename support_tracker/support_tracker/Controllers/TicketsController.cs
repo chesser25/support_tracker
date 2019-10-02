4d81819e124c67a953865c33f3ab51f4da5df79c
Andrew <andrewchess25@gmail.com>
2019-10-03 00:10:17 +0300

    Tickets controller was added; create (get and post) actions were added.

---
+++ support_tracker/support_tracker/Controllers/TicketsController.cs
@@ -0,0 +1,20 @@
+﻿using support_tracker.Models;
+using System.Web.Mvc;
+
+namespace support_tracker.Controllers
+{
+    public class TicketsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Ticket ticket)
+        {
+            return View();
+        }
+    }
+}
