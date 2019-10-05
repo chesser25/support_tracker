8f7572ad6a41ed0d04781fa350c38f3902bc29e4
Andrew <andrewchess25@gmail.com>
2019-10-05 20:07:57 +0300

    Departments repository was injected into Tickets controller.

--- support_tracker/support_tracker/Controllers/TicketsController.cs
+++ support_tracker/support_tracker/Controllers/TicketsController.cs
@@ -1,10 +1,18 @@
 ﻿using support_tracker.Models;
 using System.Web.Mvc;
+using support_tracker.AbstractRepos;
 
 namespace support_tracker.Controllers
 {
     public class TicketsController : Controller
     {
+        private IGenericRepository<Department> departmentsRepository;
+
+        public TicketsController(IGenericRepository<Department> departmentsRepository)
+        {
+            this.departmentsRepository = departmentsRepository;
+        }
+
         [HttpGet]
         public ActionResult Create()
         {
