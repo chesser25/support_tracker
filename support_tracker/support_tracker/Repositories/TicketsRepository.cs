3b533133d57dfa5f3d1188670e0ba2fc3fabc4bb
Andrew <andrewchess25@gmail.com>
2019-10-06 01:16:56 +0300

    Tickets repository was added.

---
+++ support_tracker/support_tracker/Repositories/TicketsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using support_tracker.AbstractRepos;
+
+namespace support_tracker.Repositories
+{
+    public class TicketsRepository<T,C> : ITicketsRepository<T>
+        where T : class
+        where C : DbContext
+    {
+        private C dataContext;
+        private DbSet<T> dbSet;
+
+        public TicketsRepository(C context)
+        {
+            this.dataContext = context;
+            this.dbSet = context.Set<T>();
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+
+        public virtual void Create(T item)
+        {
+            dbSet.Add(item);
+        }
+    }
+}
