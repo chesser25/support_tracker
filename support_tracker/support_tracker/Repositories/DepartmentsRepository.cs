69d1a0367a9c8a2745651bfd9a36666bcd010d63
Andrew <andrewchess25@gmail.com>
2019-10-05 18:50:26 +0300

    DepartmentRepository was added.

---
+++ support_tracker/support_tracker/Repositories/DepartmentsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using support_tracker.AbstractRepos;
+
+namespace support_tracker.Repositories
+{
+    public class DepartmentsRepository<T,C> : IGenericRepository<T>
+        where T : class
+        where C : DbContext
+    {
+        private C dataContext;
+        private DbSet<T> dbSet;
+
+        public DepartmentsRepository(C context)
+        {
+            this.dataContext = context;
+            this.dbSet = context.Set<T>();
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList();
+        }
+    }
+}
