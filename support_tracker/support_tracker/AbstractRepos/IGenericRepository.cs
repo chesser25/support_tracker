e30be87869c6070c7111fca2b0149ab4d59efc78
Andrew <andrewchess25@gmail.com>
2019-10-05 18:38:35 +0300

    Generic repository interface was added.

---
+++ support_tracker/support_tracker/AbstractRepos/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace support_tracker.AbstractRepos
+{
+    interface IGenericRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+    }
+}
