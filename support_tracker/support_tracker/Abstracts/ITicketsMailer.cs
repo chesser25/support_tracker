debae3e434e3974dbbbe5bdcdcc2cb13525d5e6e
Andrew <andrewchess25@gmail.com>
2019-10-06 18:10:38 +0300

    ITickets mailer interface was added; one omplementation was added.

---
+++ support_tracker/support_tracker/Abstracts/ITicketsMailer.cs
@@ -0,0 +1,9 @@
+﻿using support_tracker.Models;
+
+namespace support_tracker.Abstracts
+{
+    public interface ITicketsMailer
+    {
+        void Send(Ticket ticket);
+    }
+}
---
+++ support_tracker/support_tracker/Mailers/TicketsMailer.cs
@@ -0,0 +1,19 @@
+﻿using support_tracker.Abstracts;
+using System.Net.Mail;
+using support_tracker.Models;
+
+namespace support_tracker.Mailer
+{
+    public class TicketsMailer : ITicketsMailer
+    {
+        public void Send(Ticket ticket)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.To.Add(new MailAddress(ticket.CustomerEmail));
+            mailMessage.Subject = ticket.Subject;
+            mailMessage.Body = ticket.IssueDescription;
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
