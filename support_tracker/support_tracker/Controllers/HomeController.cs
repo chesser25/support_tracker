b07118ac554d4ec98adfdd4e6bcb92bdca777c95
Andrew <chess2508@mail.ru>
2019-10-07 17:24:57 +0300

    Home controller was added.

---
+++ support_tracker/support_tracker/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace support_tracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
