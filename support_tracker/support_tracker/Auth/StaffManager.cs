4f8157c2a2e6499ac6d22174f23108eec1c69ad1
Andrew <andrewchess25@gmail.com>
2019-10-07 08:04:22 +0300

    Staff manager was added to handle user creation.

---
+++ support_tracker/support_tracker/Auth/StaffManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using support_tracker.Models;
+using support_tracker.DbLayer;
+
+namespace support_tracker.Auth
+{
+    public class StaffManager : UserManager<StaffMember>
+    {
+        public StaffManager(IUserStore<StaffMember> store)
+            : base(store)
+        {
+        }
+        public static StaffManager Create(IdentityFactoryOptions<StaffManager> options,
+                                                IOwinContext context)
+        {
+            DataContext db = context.Get<DataContext>();
+            StaffManager manager = new StaffManager(new UserStore<StaffMember>(db));
+            return manager;
+        }
+    }
+}
