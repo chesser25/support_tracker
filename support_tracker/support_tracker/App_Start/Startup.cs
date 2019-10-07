f462873b3bb3b2fdca5872566283a35d37c96730
Andrew <chess2508@mail.ru>
2019-10-07 12:08:07 +0300

    DataContext has been taken from ninject container and added to owin context.

--- support_tracker/support_tracker/App_Start/Startup.cs
+++ support_tracker/support_tracker/App_Start/Startup.cs
@@ -3,6 +3,9 @@ using Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.AspNet.Identity;
 using support_tracker.Auth;
+using support_tracker.DbLayer;
+using System.Data.Entity;
+using System.Web.Mvc;
 
 [assembly: OwinStartup(typeof(support_tracker.Startup))]
 
@@ -10,8 +13,15 @@ namespace support_tracker
 {
     public class Startup
     {
+        private DbContext dbContext;
+        public Startup()
+        {
+            dbContext = DependencyResolver.Current.GetService<DbContext>();
+        }
+
         public void Configuration(IAppBuilder app)
         {
+            app.CreatePerOwinContext<DataContext>(() => dbContext as DataContext);
             app.CreatePerOwinContext<StaffManager>(StaffManager.Create);
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
