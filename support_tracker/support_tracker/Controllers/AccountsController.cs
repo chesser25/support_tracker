9d9196d309609b3b0bb41e9f2d6b5db5429f70b9
Andrew <andrewchess25@gmail.com>
2019-10-07 08:07:17 +0300

    support_tracker/Controllers/AccountsController.cs

---
+++ support_tracker/support_tracker/Controllers/AccountsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using support_tracker.Auth;
+using support_tracker.Models;
+
+namespace support_tracker.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private StaffManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<StaffManager>();
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(StaffMember model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await UserManager.CreateAsync(model, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
