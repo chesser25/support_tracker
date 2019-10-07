8e24071b77994fcbb303caf2da434a9ded62c869
Andrew <andrewchess25@gmail.com>
2019-10-07 08:05:37 +0300

    DataContext was changed to inherit from IdentityDbContext instead of DbContext.

--- support_tracker/support_tracker/Data_Layer/DataContext.cs
+++ support_tracker/support_tracker/Data_Layer/DataContext.cs
@@ -1,18 +1,19 @@
 ﻿using System.Data.Entity;
 using support_tracker.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace support_tracker.DbLayer
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<StaffMember>
     {
         public DataContext() : base()
         {
             Database.SetInitializer(new DbInitializer());
         }
+
         public DbSet<Ticket> Tickets { get; set; }
         public DbSet<TicketStatus> TicketStatuses { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Comment> Comments { get; set; }
-        public DbSet<Admin> Admins { get; set; }
     }
 }
