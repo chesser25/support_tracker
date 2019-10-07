76aed3c1b1babca6a725d87231f0ade90f87718b
Andrew <chess2508@mail.ru>
2019-10-07 15:48:22 +0300

    Login get and post methods were added.

--- support_tracker/support_tracker/Controllers/AccountsController.cs
+++ support_tracker/support_tracker/Controllers/AccountsController.cs
@@ -1,10 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using support_tracker.Auth;
 using support_tracker.Models;
+using support_tracker.ViewModels;
 
 namespace support_tracker.Controllers
 {
@@ -18,6 +22,14 @@ namespace support_tracker.Controllers
             }
         }
 
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
         [HttpGet]
         public ActionResult Register()
         {
@@ -25,14 +37,15 @@ namespace support_tracker.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Register(StaffMember model)
+        public async Task<ActionResult> Register(RegisterModel model)
         {
             if (ModelState.IsValid)
             {
-                IdentityResult result = await StaffManager.CreateAsync(model, model.Password);
+                StaffMember staffMember = new StaffMember { UserName = model.UserName, Email = model.Email };
+                IdentityResult result = await StaffManager.CreateAsync(staffMember, model.Password);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Login", "Account");
+                    return RedirectToAction("Login", "Accounts");
                 }
                 else
                 {
@@ -43,6 +56,46 @@ namespace support_tracker.Controllers
                 }
             }
             return View(model);
+        }
+
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                StaffMember user = await StaffManager.FindAsync(model.Email, model.Password);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Wrong login or password.");
+                }
+                else
+                {
+                    ClaimsIdentity claim = await StaffManager.CreateIdentityAsync(user,
+                                            DefaultAuthenticationTypes.ApplicationCookie);
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+                    if (String.IsNullOrEmpty(returnUrl))
+                        return RedirectToAction("Index", "Home");
+                    return Redirect(returnUrl);
+                }
+            }
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Login");
         }
     }
 }
