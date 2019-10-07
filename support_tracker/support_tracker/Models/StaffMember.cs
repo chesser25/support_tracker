bbde3d66511df3351b201fb4938f9dcaa136aaff
Andrew <chess2508@mail.ru>
2019-10-07 15:46:41 +0300

    StaffMember model was refactored; Login and register view models were added.

--- support_tracker/support_tracker/Models/StaffMember.cs
+++ support_tracker/support_tracker/Models/StaffMember.cs
@@ -7,23 +7,6 @@ namespace support_tracker.Models
     public class StaffMember : IdentityUser
     {
         public int StaffMemberId { get; set; }
-
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        public override string Email { get; set; }
-
-        [Required]
-        public override string UserName { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-        [Required]
-        [Compare("Password", ErrorMessage = "Passwords are not equal")]
-        [DataType(DataType.Password)]
-        public string PasswordConfirm { get; set; }
-
         public ICollection<Ticket> Tickets { get; set; }
     }
 }
---
+++ support_tracker/support_tracker/ViewModels/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace support_tracker.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords are not equal")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+}
