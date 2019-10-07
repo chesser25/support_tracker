bd71e8542a3758765fcfdcd33dd77c36a7a1b4c5
Andrew <andrewchess25@gmail.com>
2019-10-07 08:06:56 +0300

    Configuration of StaffManager creation and cookie setup was added in Startup class.

---
+++ support_tracker/support_tracker/App_Start/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using support_tracker.Auth;
+
+[assembly: OwinStartup(typeof(support_tracker.Startup))]
+
+namespace support_tracker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<StaffManager>(StaffManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
