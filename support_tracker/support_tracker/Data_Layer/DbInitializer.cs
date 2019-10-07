1bb9e81b907c5b92486590f53da0805be57ac90a
Andrew <chess2508@mail.ru>
2019-10-07 22:21:16 +0300

    Mock ticket statuses were added via db initialization.

--- support_tracker/support_tracker/Data_Layer/DbInitializer.cs
+++ support_tracker/support_tracker/Data_Layer/DbInitializer.cs
@@ -9,11 +9,25 @@ namespace support_tracker.DbLayer
     {
         protected override void Seed(DataContext context)
         {
-            IList<Department> departments = new List<Department>();
-            departments.Add(new Department { DepartmentName = "Technicians" });
-            departments.Add(new Department { DepartmentName = "Repairmans" });
-            departments.Add(new Department { DepartmentName = "Support" });
+            // Initialize departments
+            IList<Department> departments = new List<Department>()
+            {
+                new Department { DepartmentName = "Technicians" },
+                new Department { DepartmentName = "Repairmans" },
+                new Department { DepartmentName = "Support" }
+            };
             context.Departments.AddRange(departments);
+
+            // Initialize ticket statuses
+            IList<TicketStatus> ticketStatuses = new List<TicketStatus>()
+            {
+                new TicketStatus { Status = "Waiting for Staff Response" },
+                new TicketStatus { Status = "Waiting for Customer" },
+                new TicketStatus { Status = "On Hold" },
+                new TicketStatus { Status = "Cancelled" },
+                new TicketStatus { Status = "Completed" }
+            };
+            context.TicketStatuses.AddRange(ticketStatuses);
             base.Seed(context);
         }
     }
