f8ca9d215ecffe4547daef66ba6aa62f06dd5e78
Andrew <chess2508@mail.ru>
2019-10-07 22:26:29 +0300

    Unnecessary properties were removed.

--- support_tracker/support_tracker/Models/StaffMember.cs
+++ support_tracker/support_tracker/Models/StaffMember.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace support_tracker.Models
 {
     public class StaffMember : IdentityUser
     {
-        public int StaffMemberId { get; set; }
         public ICollection<Ticket> Tickets { get; set; }
     }
 }
