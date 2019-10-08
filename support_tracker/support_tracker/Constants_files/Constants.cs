e4a0d48fd87347b74d58309906dc2349e3df3da1
Andrew <chess2508@mail.ru>
2019-10-08 22:24:10 +0300

    Constants were added to the project.

---
+++ support_tracker/support_tracker/Constants_files/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace support_tracker.Constants_files
+{
+    public class Constants
+    {
+        public const string MAIL_HEADER = "The mail from support team!";
+        public const string MAIL_SUBJECT = "Thanks for your request. We will process it asap.";
+    }
+}
