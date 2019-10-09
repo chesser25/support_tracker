f28cd1a4559ad6dc89cbad3278c9353efd086dc1
Andrew <chess2508@mail.ru>
2019-10-09 17:14:58 +0300

    Tickets repository was injected; getting all messages belonging to a current ticket was done; send these messages to list of messages view was implemented.

--- support_tracker/support_tracker/Controllers/MessageController.cs
+++ support_tracker/support_tracker/Controllers/MessageController.cs
@@ -1,26 +1,26 @@
 ﻿using System.Web.Mvc;
 using support_tracker.Abstracts;
 using support_tracker.Models;
+using System.Linq;
 
 namespace support_tracker.Controllers
 {
     public class MessageController : Controller
     {
         private readonly IMessageRepository<Message> messageRepository;
+        private readonly ITicketsRepository<Ticket> ticketsRepository;
 
-        public MessageController(IMessageRepository<Message> messageRepository)
+        public MessageController(IMessageRepository<Message> messageRepository, ITicketsRepository<Ticket> ticketsRepository)
         {
             this.messageRepository = messageRepository;
+            this.ticketsRepository = ticketsRepository;
         }
 
-        public PartialViewResult GetMessages()
+        public ActionResult GetMessages(int ticketId)
         {
-            if (Request.IsAjaxRequest())
-            {
-                var messages = messageRepository.GetAll();
-                return PartialView("MessagePartial", messages);
-            }
-            return null;
+            var ticket = ticketsRepository.Get(ticketId);
+            var messages = ticket.Messages?.ToList();
+            return View("MessagePartial", messages);
         }
     }
 }
