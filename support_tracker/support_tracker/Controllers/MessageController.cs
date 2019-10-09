82e1bffc912ef14f8d9a087e5c7c28e3bb3e86db
Andrew <chess2508@mail.ru>
2019-10-09 14:37:33 +0300

    Message repository was injected into message controller; get all messages action was setup.

--- support_tracker/support_tracker/Controllers/MessageController.cs
+++ support_tracker/support_tracker/Controllers/MessageController.cs
@@ -1,13 +1,26 @@
 ﻿using System.Web.Mvc;
+using support_tracker.Abstracts;
+using support_tracker.Models;
 
 namespace support_tracker.Controllers
 {
     public class MessageController : Controller
     {
-        // GET: Message
-        public ActionResult Index()
+        private readonly IMessageRepository<Message> messageRepository;
+
+        public MessageController(IMessageRepository<Message> messageRepository)
         {
-            return View();
+            this.messageRepository = messageRepository;
+        }
+
+        public PartialViewResult GetMessages()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                var messages = messageRepository.GetAll();
+                return PartialView("MessagePartial", messages);
+            }
+            return null;
         }
     }
 }
