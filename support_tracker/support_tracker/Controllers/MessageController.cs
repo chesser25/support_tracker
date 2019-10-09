1a8cf7994b9aabe155ac28da3e8b59c0fe50ed7d
Andrew <chess2508@mail.ru>
2019-10-09 14:12:32 +0300

    MessageController was added.

---
+++ support_tracker/support_tracker/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace support_tracker.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
