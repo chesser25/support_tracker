1380277abe8de079308914903fb79a283cebf828
Andrew <chess2508@mail.ru>
2019-10-10 23:27:05 +0300

    Constant property was added to use as a message when user gets an mail for a support response on his ticket.

--- support_tracker/support_tracker/Constants_files/Constants.cs
+++ support_tracker/support_tracker/Constants_files/Constants.cs
@@ -7,5 +7,6 @@
         public const string MESSAGE_MAX_SYMBOLS_COUNT = "Max symbols count is 30.";
         public const string FIELD_REQUIRED = "This field can not be empty.";
         public const string TICKET_STATUS_CHANGED = "Ticket status has been changed!";
+        public const string SUPPORT_RESPONSE_ON_TICKET = "There are some news for your ticket. Please take a look at link below.";
     }
 }
