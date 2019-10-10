1715f8e12255bd2e193cde091ad9764ae2ecb281
Andrew <chess2508@mail.ru>
2019-10-10 19:13:51 +0300

    Alert view model was added.

---
+++ support_tracker/support_tracker/ViewModels/AlertModel.cs
@@ -0,0 +1,9 @@
+﻿namespace support_tracker.ViewModels
+{
+    public enum Style { primary, success, danger }
+    public class AlertModel
+    {
+        public string Style { get; set; }
+        public string Message { get; set; }
+    }
+}
