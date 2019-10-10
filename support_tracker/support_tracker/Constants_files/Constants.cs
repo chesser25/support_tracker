f62f52755be330d155c9b7416e5f48e53e27d68a
Andrew <chess2508@mail.ru>
2019-10-10 15:52:28 +0300

    Message text validation was setup in model. Constants error props were added.

--- support_tracker/support_tracker/Constants_files/Constants.cs
+++ support_tracker/support_tracker/Constants_files/Constants.cs
@@ -4,5 +4,7 @@
     {
         public const string MAIL_HEADER = "The mail from support team!";
         public const string MAIL_SUBJECT = "Thanks for your request. We will process it asap.";
+        public const string MESSAGE_MAX_SYMBOLS_COUNT = "Max symbols count is 30.";
+        public const string FIELD_REQUIRED = "This field can not be empty.";
     }
 }
--- support_tracker/support_tracker/Models/Message.cs
+++ support_tracker/support_tracker/Models/Message.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using support_tracker.Constants_files;
 
 namespace support_tracker.Models
 {
     public class Message
     {
         public int Id { get; set; }
+
+        [StringLength(30, MinimumLength = 3, ErrorMessage = Constants.MESSAGE_MAX_SYMBOLS_COUNT)]
+        [Required(ErrorMessage = Constants.FIELD_REQUIRED)]
         public string Text { get; set; }
+
         public DateTime CreationDate { get; set; }
 
         // Foreign keys
