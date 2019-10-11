15c343f1d928d9d2f09e267af43f10f2471862bc
Andrew <chess2508@mail.ru>
2019-10-11 16:28:55 +0300

    Auth helper was added to get staff manager and auth manager.

---
+++ support_tracker/support_tracker/Auth/AuthHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace support_tracker.Auth
+{
+    public class AuthHelper
+    {
+        public StaffManager GetStaffManagerFromOwinContext => HttpContext.Current.GetOwinContext().GetUserManager<StaffManager>();
+        public IAuthenticationManager GetAuthenticationManagerFromOwinContext => HttpContext.Current.GetOwinContext().Authentication;
+    }
+}
